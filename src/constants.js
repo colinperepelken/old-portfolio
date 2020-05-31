b39e1147f0958abebd6503ff56bac48bb667e887
Colin Perepelken <colin@perepelken.ca>
2020-05-31 11:27:30 -0700

    Move projectData to constants file

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,51 @@
+import wclnImage from './assets/bike_profile.jpg';
+
+
 export const githubLink = "https://github.com/colinperepelken";
 export const linkedinLink = "https://linkedin.com/in/colin-perepelken"
+
+
+export const projectData = [
+    {
+        title: "WCLN LTI provider solution",
+        content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
+        image: wclnImage,
+        github: 'https://github.com/wcln/moodle-local_lti'
+    },
+    {
+        title: "WCLN LTI provider solution",
+        content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
+        image: wclnImage,
+        github: 'https://github.com/wcln/moodle-local_lti'
+    },
+    {
+        title: "WCLN LTI provider solution",
+        content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
+        image: wclnImage,
+        github: 'https://github.com/wcln/moodle-local_lti'
+    },
+    {
+        title: "WCLN LTI provider solution",
+        content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
+        image: wclnImage,
+        github: 'https://github.com/wcln/moodle-local_lti'
+    },
+    {
+        title: "WCLN LTI provider solution",
+        content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
+        image: wclnImage,
+        github: 'https://github.com/wcln/moodle-local_lti'
+    },
+    {
+        title: "WCLN LTI provider solution",
+        content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
+        image: wclnImage,
+        github: 'https://github.com/wcln/moodle-local_lti'
+    },
+    {
+        title: "WCLN LTI provider solution",
+        content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
+        image: wclnImage,
+        github: 'https://github.com/wcln/moodle-local_lti'
+    },
+];
--- src/views/ProjectList.js
+++ src/views/ProjectList.js
@@ -7,9 +7,10 @@ import {
     CardMedia,
     CardActions,
     Button,
+    Box
 } from "@material-ui/core";
 
-import wclnImage from '../assets/bike_profile.jpg';
+import { projectData } from '../constants.js';
 
 import { theme } from "../theme";
 import { makeStyles } from "@material-ui/core/styles";
@@ -18,86 +19,48 @@ const useStyles = makeStyles(theme => ({
     cardImage: {
         height: 140
     },
+    projectsBox: {
+        maxWidth: "1100px",
+        marginLeft: "auto",
+        marginRight: "auto"
+    }
 }));
 
 const ProjectList = () => {
 
     const classes = useStyles(theme);
 
-    const projectData = [
-        {
-            title: "WCLN LTI provider solution",
-            content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
-            image: wclnImage,
-            github: 'https://github.com/wcln/moodle-local_lti'
-        },
-        {
-            title: "WCLN LTI provider solution",
-            content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
-            image: wclnImage,
-            github: 'https://github.com/wcln/moodle-local_lti'
-        },
-        {
-            title: "WCLN LTI provider solution",
-            content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
-            image: wclnImage,
-            github: 'https://github.com/wcln/moodle-local_lti'
-        },
-        {
-            title: "WCLN LTI provider solution",
-            content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
-            image: wclnImage,
-            github: 'https://github.com/wcln/moodle-local_lti'
-        },
-        {
-            title: "WCLN LTI provider solution",
-            content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
-            image: wclnImage,
-            github: 'https://github.com/wcln/moodle-local_lti'
-        },
-        {
-            title: "WCLN LTI provider solution",
-            content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
-            image: wclnImage,
-            github: 'https://github.com/wcln/moodle-local_lti'
-        },
-        {
-            title: "WCLN LTI provider solution",
-            content: "I built a custom LTI provider solution for Western Canadian Learning Network to distribute course content to school districts throughout Western Canada. It handles millions of resource requests every year.",
-            image: wclnImage,
-            github: 'https://github.com/wcln/moodle-local_lti'
-        },
-    ];
-
     return (
-        <Grid container justify="center" alignItems="center" spacing={3}>
-            {projectData.map((project) => {
-                return (
-                    <Grid item xs={12} sm={6} md={3}>
-                        <Card>
-                            <CardMedia 
-                                className={classes.cardImage}
-                                image={project.image}
-                                title={project.title}
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    {project.title}
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary" component="p">
-                                    {project.content}
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small" color="secondary" href={project.github}>
-                                    View on GitHub
-                                </Button>
-                            </CardActions>
-                        </Card> 
-                    </Grid>
-                );
-            })}
-        </Grid>
+        <Box className={classes.projectsBox}>
+            <Grid container justify="center" alignItems="center" spacing={3}>
+                {projectData.map((project) => {
+                    return (
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Card>
+                                <CardMedia 
+                                    className={classes.cardImage}
+                                    image={project.image}
+                                    title={project.title}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        {project.title}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        {project.content}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="secondary" href={project.github}>
+                                        View on GitHub
+                                    </Button>
+                                </CardActions>
+                            </Card> 
+                        </Grid>
+                    );
+                })}
+            </Grid>
+        </Box>
     );
 }
 
